d1826895b4688d4296fdb499c8fadd5338c5d633
One <One@L11-mainll.Oldmainlab.local>
2017-10-06 12:52:38 -0400

    new notes

--- scratch.R
+++ scratch.R
@@ -10,3 +10,15 @@ for(i in 1:36574){
   }
   print(i)
 }
+
+df<-deaths%>%
+  filter(str_detect(deaths$INCIDENT_DATE,"\\d{8}")==TRUE)%>%
+  group_by(INCIDENT_DATE)%>%
+  summarize(num_deaths=n())%>%
+  mutate(date=ymd(INCIDENT_DATE))%>%
+  filter(date<="1953-07-27")%>%
+  select(INCIDENT_DATE,num_deaths,date)
+
+ggplot()+
+  geom_line(data=df,aes(x=date,y=num_deaths))+
+  scale_x_date(date_breaks='3 months',date_labels = "%b %y")
